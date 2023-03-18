e6e1c69db07d00b3c027ae65efb38b4fac5f4f89
Robert Hoanca <robert.hoanca@mymetasoftware.com>
2023-03-18 12:30:09 +0100

    reactivate localsaves

--- project-rekord/src/app/components/game/game.component.ts
+++ project-rekord/src/app/components/game/game.component.ts
@@ -102,7 +102,7 @@ export class GameComponent implements OnInit {
     this.gameService.camera = this.camera;
     this.gameService.cameraControls = this.cameraControls;
     this.gameService.setCameraPosition(this.camera, this.gameService.players[this.gameService.turn].pawn.position[0],this.gameService.players[this.gameService.turn].pawn.position[1],this.gameService.players[this.gameService.turn].pawn.position[2], 2500, 5, false)   
-    //this.activateLocalSave();
+    this.activateLocalSave();
     this.gameService.gameScene = this.scene._objRef;
     this.gamePhysicsService.initWorld();
     this.gamePhysicsService.showDiceResultDialogRef = this.showDiceResultDialogRef;
