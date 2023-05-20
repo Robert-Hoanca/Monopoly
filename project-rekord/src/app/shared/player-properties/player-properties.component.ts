f3a9a286873f1fa5547a0eb3c40a96b5cd562da3
Robert Hoanca <robert.hoanca@mymetasoftware.com>
2023-05-20 19:51:40 +0200

    restyled buttons and bat, changinf players info dialog

--- project-rekord/src/app/shared/player-properties/player-properties.component.ts
+++ project-rekord/src/app/shared/player-properties/player-properties.component.ts
@@ -11,18 +11,34 @@ export class PlayerPropertiesComponent implements OnInit {
   @Input() playerId: string = '';
   @Input() imInExchange: boolean = false;
 
+  playerProps:Array<any> = [];
+  dividedPlayerProps:Array<any> = [];
+  playerGetOutCards:number = 0;
 
   constructor(public gameService: GameService) { }
 
   ngOnInit(): void {
+    this.getPlayerProps()
   }
 
   getPlayerProps(){
-    return  this.gameService.sortProperties(this.gameService.gameTable.cards.filter((prop: { owner: any; }) => prop.owner == this.playerId));
+    this.playerProps = this.gameService.sortProperties(this.gameService.gameTable.cards.filter((prop: { owner: any; }) => prop.owner == this.playerId))
+   
+    const districts:Array<any> = [];
+
+    this.playerProps.forEach(prop => {
+      if(!districts.includes(prop.district)){
+        districts.push(prop.district)
+      }
+    });
+
+    districts.forEach(district => {
+      this.dividedPlayerProps.push(this.playerProps.filter(card => card.district === district))
+    });
   }
 
   getPlayerGetOutCards(){
-    return  this.gameService.players.find(player => player.id === this.playerId).prison.getOutCards;
+    this.playerGetOutCards = this.gameService.players.find(player => player.id === this.playerId).prison.getOutCards;
   }
 
   selectProperty(property:any){
