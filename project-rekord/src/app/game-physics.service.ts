3cca8a2688cce2b7b65aac0aed383d791d4f4ca1
Robert Hoanca <robert.hoanca@mymetasoftware.com>
2023-05-27 11:52:09 +0200

    changed dice gravity

--- project-rekord/src/app/game-physics.service.ts
+++ project-rekord/src/app/game-physics.service.ts
@@ -24,7 +24,7 @@ export class GamePhysicsService {
   constructor(public gameService: GameService, private dialog: MatDialog) {}
 
   initWorld() {
-    this.world.gravity.set(0, -20, 0); // -9.82 m/s²
+    this.world.gravity.set(0, -40, 0); // -9.82 m/s² 
     this.world.defaultContactMaterial.restitution = 0.3;
     this.createDiceCase();
     if (this.diceArray.length) {
